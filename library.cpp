aded58798f178aacd10d6482353a8621e9dd68a2
Daniil <danast_2@mail.ru>
2024-02-16 06:15:01 +0300

    16.02.2024/06:15

--- library.cpp
+++ library.cpp
@@ -2,6 +2,17 @@
 
 #include <iostream>
 
+void Manage_crypt::encrypt_file() {
+
+}
+
+void Manage_crypt::decrypt_file() {
+
+}
+
+string Manage_crypt::get_password() {}
+
+string Manage_crypt::get_path_to_file() {}
 
 void hello() {
     std::cout << "Hello, World!" << std::endl;
--- library.h
+++ library.h
@@ -11,7 +11,10 @@ private:
     string path_to_file;
 public:
     Manage_crypt(string &pass, string &path): password(pass), path_to_file(path){}
-
+    void encrypt_file();
+    void decrypt_file();
+    string get_password();
+    string get_path_to_file();
 };
 
 void hello();
