cacd457ba2d171aa9edd7ec1482f579085499037
Daniil <danast_2@mail.ru>
2024-02-16 06:11:10 +0300

    16.02.2024/06:11

--- library.h
+++ library.h
@@ -1,6 +1,19 @@
 #ifndef FILE_ENCRYPTION_LIBRARY_H
 #define FILE_ENCRYPTION_LIBRARY_H
 
+#include <string>
+
+using std::string;
+
+class Manage_crypt{
+private:
+    string password;
+    string path_to_file;
+public:
+    Manage_crypt(string &pass, string &path): password(pass), path_to_file(path){}
+
+};
+
 void hello();
 
 #endif //FILE_ENCRYPTION_LIBRARY_H
